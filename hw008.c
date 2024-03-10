68722d5d65bc48d92914db0a09570602670d3f46
easn <a35744830@gmail.com>
2024-03-10 10:35:35 +0800

    2024/3/10

--- hw008.c
+++ hw008.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
+
+int abs(int tmp){
+    if(tmp>0){return tmp;}
+    return -1*tmp;
+}
+
+int catch_int(char* tmp){
+    int fake=0,co=0;
+    while('0'<=(int)*(tmp+co) && (int)*(tmp+co)<='9'){
+        fake=fake*10+((int)*(tmp+co)-'0');
+        co++;
+    }
+    return fake;
+}
 
 void gary(int n,int k,char ans[],int co){
     if(n==1){
@@ -18,18 +31,24 @@ void gary(int n,int k,char ans[],int co){
 }
 
 int main(){
-    int n=0,k;
+    int n,k;
     while(1){
         scanf("%d",&n);
         if(n==-1){break;}
         scanf("%d",&k);
-        if(1<=n && n<=8){
+        if(1<=n && n<=10){
             char ans[n+1];
             gary(n,k,ans,0);
-            for(int i=0;i<n-(int)(log10(abs(atoi(ans)))+1);i++){
-                printf("0");
+            if(catch_int(ans)!=0){
+                for(int i=0;i<n-(int)(log10(abs(catch_int(ans)))+1);i++){
+                    printf("0");
+                }
             }
-            printf("%d\n",atoi(ans));
+            else{
+                for(int i=0;i<n-1;i++)
+                    printf("0");
+            }
+            printf("%d\n",catch_int(ans));
         }
         else{
             printf("0");
