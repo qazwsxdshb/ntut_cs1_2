8bc783bb448ce03a03c462f3cdad0d249fe55bf7
easn <a35744830@gmail.com>
2024-03-31 14:32:49 +0800

    2024/03/31 Ysaÿe Sonata OP27

--- hw016.c
+++ hw016.c
@@ -5,9 +5,7 @@ int main(){
     scanf("%d",&co);
     for(int i=0;i<co;i++){scanf("%d",&arr[i]);}
     for(int i=0;i<co;i++){
-        for(int u=i+1;u<co;u++){
-            ans+=arr[i]>arr[u]?1:0;
-        }
+        for(int u=i+1;u<co;u++){ans+=arr[i]>arr[u]?1:0;}
     }
     printf("%d",ans);
 }
