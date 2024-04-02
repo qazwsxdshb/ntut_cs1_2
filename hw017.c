01f71579a87e0d824c7480a84b938d0901ac604b
easn <a35744830@gmail.com>
2024-04-02 14:04:21 +0800

    2024/04/02 Piazzolla NightClub 1960

--- hw017.c
+++ hw017.c
@@ -3,7 +3,7 @@
 
 void sol(char a[100],int size,int *i,int x,int y,int ans[10][10]){
     int co=0;
-    while(*i<strlen(a)){
+    while((*i)<strlen(a)){
         if(a[(*i)++]=='2'){sol(a,size/2,i,x,y,ans);}
         else if(a[(*i)-1]=='1'){
             for(int xx=0;xx<size*size;xx++){
@@ -23,10 +23,8 @@ int main(){
     scanf("%s",a);
     scanf("%d",&size);
     sol(a,size,&i,0,0,ans);
-    for(int x=0;x<10;x++){
-       for(int y=0;y<10;y++){
-            if(ans[x][y]==1){printf("%d,%d\n",x,y);d=1;}
-        }
+    for(int x=0;x<100;x++){
+        if(ans[x/10][x%10]==1){printf("%d,%d\n",x/10,x%10);d=1;}
     }
     if(d==0){printf("all white");}
 }
