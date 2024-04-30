6a234883b290ebfea2b0aa7835123e6c0288351f
easn <a35744830@gmail.com>
2024-04-30 18:22:08 +0800

    Wagner WWV 94

--- hw021.c
+++ hw021.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int tok(char a[200],char b[10][200]){
+int tok(char a[20],char b[10][20]){
     int co=0,x=0,i=0;
     while(a[i]!='\0'){
         if(a[i]==' '){x++;co=0;i++;}
@@ -10,38 +10,35 @@ int tok(char a[200],char b[10][200]){
     return x+1;
 }
 
+//互補對
 int fun(char *a,char b[200]){
-    //互補對
-    char *token,bb[200]={0};
-    memccpy(bb,b,'\0',strlen(b));
-    token=strtok(bb," ");
-    
-    while(token != NULL){
-        if(strcmp(a,token)==0){return 1;}
-        token=strtok(NULL," ");
+    char bb[10][20]={0};
+    int x=0;
+    x=tok(b,bb);
+    for(int i=0;i<x+1;i++){
+        if(strcmp(a,bb[i])==0){return 1;}
     }
     return 0;
 }
 
 int sol(char a[200],char b[200],char s[200]){
-    char aa[10][200]={0},ss[10][200]={0};
-    int x;
-    x=tok(a,aa);
+    char tmp[10][20]={0},tmp1[10][20]={0};
+    int x=0;
+    x=tok(a,tmp);
     for(int i=0;i<x;i++){
-        if(fun(aa[i],b)){return 0;}
+        if(fun(tmp[i],b)){return 0;}
     }
-
-    x=tok(s,ss);
-    for(int i=0;i<x;i++){
-        if(fun(ss[i],a) || fun(ss[i],b)){NULL;}
-        else{return 0;}
+    
+    x=tok(s,tmp1);
+    for(int i=0;i<x+1;i++){
+        if(!(fun(tmp1[i],a) || fun(tmp1[i],b))){return 0;}
     }
     return 1;
 }
 
 int main(){
-    char s[200]={0},S1[10][200]={0},tmp;
-    int co,ans=0,co1=0;
+    char s[200]={0},S1[100][200]={0},tmp=0;
+    int co=0,ans=0,co1=0;
     while(1){
         scanf("%c",&tmp);
         if(tmp=='\n'){break;}
@@ -56,6 +53,7 @@ int main(){
             S1[i][co1++]=tmp;
         }
     }
+    char sss[100][200]={0};
     for(int i=0;i<co;i++){
         for(int u=i+1;u<co;u++){
             ans+=sol(S1[i],S1[u],s);
