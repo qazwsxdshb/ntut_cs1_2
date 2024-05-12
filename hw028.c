1eba02bf0a5ba9d4c1b2cdb8d3d67501ae2be37c
easn <a35744830@gmail.com>
2024-05-12 20:34:46 +0800

    tmp

---
+++ hw028.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define provalue(project) int(*proname){}
+
+typedef struct _project{
+    int day;
+    int wait;
+    int node[100];
+}project;
+
+
+int main(){
+    int co;
+    scanf("%d",&co);
+    project pro[];
+    for(int i=0;i<co;i++){
+        getchar();
+
+    }
+}
