8f2aeef94f844b05efc460779c8fc82407c4e5b0
easn <a35744830@gmail.com>
2024-05-13 16:15:21 +0800

    2024/05/13 Kreisler Praeludium and Allegro in the Style of Pugnani

--- hw024.c
+++ hw024.c
@@ -5,18 +5,32 @@ char name[50];
 int score;
 }student;
 
-int main(){
-    int co,max=0,maxpointer,sum=0;
-    student stu[10];
-    scanf("%d",&co);
+void input(student stu[], int co){
     for(int i=0;i<co;i++){
         scanf("%s",stu[i].name);
         scanf("%d",&stu[i].score);
     }
+}
+
+void getHigh(student stu[],int co){
+    int max=0,maxpointer;
     for(int i=0;i<co;i++){
         if(max<stu[i].score){max=stu[i].score;maxpointer=i;}
-        sum+=stu[i].score;
     }
     printf("%s\n",stu[maxpointer].name);
-    printf("%d",sum/co);
+}
+
+int getAverage(student stu[], int co){
+    int sum=0;
+    for(int i=0;i<co;i++){sum+=stu[i].score;}
+    return sum/co;
+}
+
+int main(){
+    int co;
+    student stu[10];
+    scanf("%d",&co);
+    input(stu,co);
+    getHigh(stu,co);
+    printf("%d",getAverage(stu,co));
 }
